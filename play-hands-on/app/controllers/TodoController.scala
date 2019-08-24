fd0f4ea79405e3b4e3d88b4f2225e975ebd9b835
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-24 20:42:02 +0900

    impl get helloworld api

--- play-hands-on/app/controllers/TodoController.scala
+++ play-hands-on/app/controllers/TodoController.scala
@@ -1,5 +1,13 @@
 package controllers
 
-class TodoController {
+import javax.inject._
+import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+class TodoController @Inject()(mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+  def helloworld() = Action {
+    implicit request: MessagesRequest[AnyContent] => Ok("Hello World")
+  }
 }
